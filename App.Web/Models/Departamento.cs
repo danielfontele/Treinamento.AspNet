b7ca0aae25b75fc80bd90403c51389bf29253cd1
Daniel Fontele <danielfontele@hotmail.com>
2022-03-15 11:43:00 -0300

    Add Models Refatoração para Helpers (@Html)

---
+++ App.Web/Models/Departamento.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace App.Web.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        [DisplayName("Descrição")]
+        public string Descricao { get; set;}
+    }
+}
---
+++ App.Web/Models/Funcionario.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace App.Web.Models
+{
+    public class Funcionario
+    {
+        public string Id { get; set; }
+        [DisplayName("Descrição")]
+        public string Descricao { get; set; }
+    }
+}
