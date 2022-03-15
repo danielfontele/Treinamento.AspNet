08eb7dc0468ee950791ba3f044f716126ed435e4
Daniel Fontele <danielfontele@hotmail.com>
2022-03-15 10:42:44 -0300

    Add Controllers

---
+++ App.Web/Controllers/DepartamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ App.Web/Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
