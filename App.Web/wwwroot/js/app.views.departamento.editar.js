81b27acbc5c791bf15d1c4ed88e9498d83c02cdb
Daniel Fontele <danielfontele@hotmail.com>
2022-03-16 16:04:59 -0300

    Starting edit departamento

--- App.Web/wwwroot/js/app.views.departamento.editar.js
+++ App.Web/wwwroot/js/app.views.departamento.editar.js
@@ -1,1 +1,21 @@
-﻿
+﻿definaNamespace("app.views.departamento.edit", {})
+
+app.views.departamento.edit = function ($el, modelParaOClientSide, options) {
+    "use strict"
+
+    if (!$el) {
+        throw "Elemento não definido."
+    }
+
+    this.$el = $el
+    this.$el.data("app.views.departamento.edit", this)
+    this.modelParaOClientSide = modelParaOClientSide
+
+    this.options = options ? jQuery.extend({}, this.options, options) : this.options
+
+    this.inicialize()
+}
+
+app.views.departamento.edit.prototype = {
+    
+}
