8560571495a69aff1832f94598c5c3c730cd08b2
Daniel Fontele <danielfontele@hotmail.com>
2022-03-17 12:07:23 -0300

    Created Helper TextBox

---
+++ App.Web/Helpers/Builders/AppTextBoxBuilder.cs
@@ -0,0 +1,29 @@
+﻿using App.Web.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System.Text.Encodings.Web;
+
+namespace App.Web.Helpers.Builders
+{
+    public class AppTextBoxBuilder : IHtmlContent
+    {
+        private IHtmlHelper _htmlHelper;
+        private string _propriedade;
+
+        public AppTextBoxBuilder(IHtmlHelper htmlHelper, string propriedade)
+        {
+            _htmlHelper = htmlHelper;
+            _propriedade = propriedade;
+        }
+
+        public void WriteTo(TextWriter writer, HtmlEncoder encoder)
+        {
+            var nomeDaView = "_textBox";
+
+            var model = new ComponenteModel { Propriedade = _propriedade };
+
+            _htmlHelper.RenderPartial(nomeDaView, model, null);
+        }
+    }
+}
---
+++ App.Web/Helpers/HelperAppTextBox.cs
@@ -0,0 +1,17 @@
+﻿using Antlr.Runtime.Misc;
+using App.Web.Helpers.Builders;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+
+namespace App.Web.Helpers
+{
+    public static class HelperAppTextBox
+    {
+        public static AppTextBoxBuilder AppTextBox<TModel, TPropriedade>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TPropriedade>> expression)
+        {
+            var propriedade = System.Web.Mvc.ExpressionHelper.GetExpressionText(expression);
+            return new AppTextBoxBuilder(htmlHelper, propriedade);
+        }
+
+    }
+}
